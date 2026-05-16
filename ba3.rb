4df5d76b5f17882ceb062ca0fa5860e17a922a69
Bruce Rannala <brannala@ucdavis.edu>
2026-05-16 11:28:25 -0700

    Fix formula: correct license SPDX identifier, fix test block

--- ba3.rb
+++ ba3.rb
@@ -3,7 +3,7 @@ class Ba3 < Formula
   homepage "https://github.com/brannala/BA3/"
   url "https://github.com/brannala/BA3/releases/download/v3.4.0/BA3-3.4.0.tar.gz"
   sha256 "69e01f326c26c0a2d7ec9c8a3680879e69997d958db693a62f5e03e1c316ebe7"
-  license "AGPL-3.0"
+  license "AGPL-3.0-or-later"
 
   depends_on "gsl"
   depends_on "htslib"
@@ -25,6 +25,6 @@ class Ba3 < Formula
       ind4 pop2 loc2 B B
     EOS
     system "#{bin}/BA3", "-i", "1000", "-o", "out.txt", "test.txt"
-    assert_predicate testpath/"out.txt", :exist?
+    assert_path_exists testpath/"out.txt"
   end
 end
